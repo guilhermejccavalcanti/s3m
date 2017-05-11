c7d5b28b1eb7634758037a06b4dcde80c17b2652
Daniel Maida <dfm2@cin.ufpe.br>
2017-05-11 15:16:40 -0300

    s3m diff reverted

--- src/br/ufpe/cin/app/JFSTMerge.java
+++ src/br/ufpe/cin/app/JFSTMerge.java
@@ -2,6 +2,7 @@ package br.ufpe.cin.app;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -35,64 +36,64 @@ import java.io.InputStreamReader;
  */
 public class JFSTMerge {
 
-    //log of activities
-    private static final Logger LOGGER = LoggerFactory.make();
+	//log of activities
+	private static final Logger LOGGER = LoggerFactory.make();
 
-    private static int conflictState = 0;
+	private static int conflictState = 0;
 
-    //command line options
-    @Parameter(names = "-f", arity = 3, description = "Files to be merged (mine, base, yours)")
-    List<String> filespath = new ArrayList<String>();
+	//command line options
+	@Parameter(names = "-f", arity = 3, description = "Files to be merged (mine, base, yours)")
+	List<String> filespath = new ArrayList<String>();
 
-    @Parameter(names = "-d", arity = 3, description = "Directories to be merged (mine, base, yours)")
-    List<String> directoriespath = new ArrayList<String>();
+	@Parameter(names = "-d", arity = 3, description = "Directories to be merged (mine, base, yours)")
+	List<String> directoriespath = new ArrayList<String>();
 
-    @Parameter(names = "-o", description = "Destination of the merged content. Optional. If no destination is specified, " + "then it will use \"yours\" as the destination for the merge. ")
-    String outputpath = "";
+	@Parameter(names = "-o", description = "Destination of the merged content. Optional. If no destination is specified, " + "then it will use \"yours\" as the destination for the merge. ")
+	String outputpath = "";
 
-    @Parameter(names = "-g", description = "Command to identify that the tool is being used as a git merge driver.")
-    public static boolean isGit = false;
+	@Parameter(names = "-g", description = "Command to identify that the tool is being used as a git merge driver.")
+	public static boolean isGit = false;
 
-    /**
+	/**
 	 * Merges merge scenarios, indicated by .revisions files. 
 	 * This is mainly used for evaluation purposes.
 	 * A .revisions file contains the directories of the revisions to merge in top-down order: 
 	 * first revision, base revision, second revision (three-way merge).
 	 * @param revisionsPath file path
 	 */
-    public MergeScenario mergeRevisions(String revisionsPath) {
-        MergeScenario scenario = null;
-        try {
-            //reading the .revisions file line by line to get revisions directories
-            List<String> listRevisions = new ArrayList<>();
-            BufferedReader reader = Files.newBufferedReader(Paths.get(revisionsPath));
-            listRevisions = reader.lines().collect(Collectors.toList());
-            if (listRevisions.size() != 3)
-                throw new Exception("Invalid .revisions file!");
-            //merging the identified directories
-            if (!listRevisions.isEmpty()) {
-                LOGGER.log(Level.INFO, "MERGING SCENARIO: " + revisionsPath);
-                System.out.println("MERGING REVISIONS: \n" + listRevisions.get(0) + "\n" + listRevisions.get(1) + "\n" + listRevisions.get(2));
-                String revisionFileFolder = (new File(revisionsPath)).getParent();
-                String leftDir = revisionFileFolder + File.separator + listRevisions.get(0);
-                String baseDir = revisionFileFolder + File.separator + listRevisions.get(1);
-                String rightDir = revisionFileFolder + File.separator + listRevisions.get(2);
-                List<FilesTuple> mergedTuples = mergeDirectories(leftDir, baseDir, rightDir, null);
-                //using the name of the revisions directories as revisions identifiers
-                scenario = new MergeScenario(revisionsPath, listRevisions.get(0), listRevisions.get(1), listRevisions.get(2), mergedTuples);
-                //printing the resulting merged codes
-                Prettyprinter.generateMergedScenario(scenario);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
-            LOGGER.log(Level.SEVERE, "", e);
-            System.exit(-1);
-        }
-        return scenario;
-    }
+	public MergeScenario mergeRevisions(String revisionsPath) {
+		MergeScenario scenario = null;
+		try {
+			//reading the .revisions file line by line to get revisions directories
+			List<String> listRevisions = new ArrayList<>();
+			BufferedReader reader = Files.newBufferedReader(Paths.get(revisionsPath));
+			listRevisions = reader.lines().collect(Collectors.toList());
+			if (listRevisions.size() != 3)
+				throw new Exception("Invalid .revisions file!");
+			//merging the identified directories
+			if (!listRevisions.isEmpty()) {
+				LOGGER.log(Level.INFO, "MERGING SCENARIO: " + revisionsPath);
+				System.out.println("MERGING REVISIONS: \n" + listRevisions.get(0) + "\n" + listRevisions.get(1) + "\n" + listRevisions.get(2));
+				String revisionFileFolder = (new File(revisionsPath)).getParent();
+				String leftDir = revisionFileFolder + File.separator + listRevisions.get(0);
+				String baseDir = revisionFileFolder + File.separator + listRevisions.get(1);
+				String rightDir = revisionFileFolder + File.separator + listRevisions.get(2);
+				List<FilesTuple> mergedTuples = mergeDirectories(leftDir, baseDir, rightDir, null);
+				//using the name of the revisions directories as revisions identifiers
+				scenario = new MergeScenario(revisionsPath, listRevisions.get(0), listRevisions.get(1), listRevisions.get(2), mergedTuples);
+				//printing the resulting merged codes
+				Prettyprinter.generateMergedScenario(scenario);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
+			LOGGER.log(Level.SEVERE, "", e);
+			System.exit(-1);
+		}
+		return scenario;
+	}
 
-    /**
+	/**
 	 * Merges directories.
 	 * @param leftDirPath (mine)
 	 * @param baseDirPath (older)
@@ -100,30 +101,30 @@ public class JFSTMerge {
 	 * @param outputDirPath can be null, in this case, the output will only be printed in the console.
 	 * @return merged files tuples
 	 */
-    public List<FilesTuple> mergeDirectories(String leftDirPath, String baseDirPath, String rightDirPath, String outputDirPath) {
-        List<FilesTuple> filesTuple = FilesManager.fillFilesTuples(leftDirPath, baseDirPath, rightDirPath, outputDirPath, new ArrayList<String>());
-        for (FilesTuple tuple : filesTuple) {
-            File left = tuple.getLeftFile();
-            File base = tuple.getBaseFile();
-            File right = tuple.getRightFile();
-            //merging the file tuple
-            MergeContext context = mergeFiles(left, base, right, null);
-            tuple.setContext(context);
-            //printing the resulting merged code
-            if (outputDirPath != null) {
-                try {
-                    Prettyprinter.generateMergedTuple(tuple);
-                } catch (PrintException pe) {
-                    System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
-                    LOGGER.log(Level.SEVERE, "", pe);
-                    System.exit(-1);
-                }
-            }
-        }
-        return filesTuple;
-    }
+	public List<FilesTuple> mergeDirectories(String leftDirPath, String baseDirPath, String rightDirPath, String outputDirPath) {
+		List<FilesTuple> filesTuple = FilesManager.fillFilesTuples(leftDirPath, baseDirPath, rightDirPath, outputDirPath, new ArrayList<String>());
+		for (FilesTuple tuple : filesTuple) {
+			File left = tuple.getLeftFile();
+			File base = tuple.getBaseFile();
+			File right = tuple.getRightFile();
+			//merging the file tuple
+			MergeContext context = mergeFiles(left, base, right, null);
+			tuple.setContext(context);
+			//printing the resulting merged code
+			if (outputDirPath != null) {
+				try {
+					Prettyprinter.generateMergedTuple(tuple);
+				} catch (PrintException pe) {
+					System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
+					LOGGER.log(Level.SEVERE, "", pe);
+					System.exit(-1);
+				}
+			}
+		}
+		return filesTuple;
+	}
 
-    /**
+	/**
 	 * Three-way semistructured merge of the given .java files.
 	 * @param left (mine) version of the file, or <b>null</b> in case of intentional empty file. 
 	 * @param base (older) version of the file, or <b>null</b> in case of intentional empty file. 
@@ -131,109 +132,87 @@ public class JFSTMerge {
 	 * @param outputFilePath of the merged file. Can be <b>null</b>, in this case, the output will only be printed in the console.
 	 * @return context with relevant information gathered during the merging process.
 	 */
-    public MergeContext mergeFiles(File left, File base, File right, String outputFilePath) {
-        FilesManager.validateFiles(left, base, right);
-        if (!isGit) {
-            System.out.println("MERGING FILES: \n" + ((left != null) ? left.getAbsolutePath() : "<empty left>") + "\n" + ((base != null) ? base.getAbsolutePath() : "<empty base>") + "\n" + ((right != null) ? right.getAbsolutePath() : "<empty right>"));
-        }
-        MergeContext context = new MergeContext(left, base, right, outputFilePath);
-        //there is no need to call specific merge algorithms in equal or consistenly changes files (fast-forward merge)
-        if (FilesManager.areFilesDifferent(left, base, right, outputFilePath, context)) {
-            try {
-                //running unstructured merge first is necessary due to future steps.
-                context.unstructuredOutput = TextualMerge.merge(left, base, right, false);
-                context.semistructuredOutput = SemistructuredMerge.merge(left, base, right, context);
-                conflictState = checkConflictState(context);
-            } catch (//textual merge must work even when semistructured not, so this exception precedes others
-            TextualMergeException //textual merge must work even when semistructured not, so this exception precedes others
-            tme) {
-                System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
-                LOGGER.log(Level.SEVERE, "", tme);
-                System.exit(-1);
-            } catch (SemistructuredMergeException sme) {
-                LOGGER.log(Level.WARNING, "", sme);
-                context.semistructuredOutput = context.unstructuredOutput;
-                conflictState = checkConflictState(context);
-            }
-        }
-        //printing the resulting merged code
-        try {
-            if (!isGit) {
-                Prettyprinter.printOnScreenMergedCode(context);
-            }
-            Prettyprinter.generateMergedFile(context, outputFilePath);
-        } catch (PrintException pe) {
-            System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
-            LOGGER.log(Level.SEVERE, "", pe);
-            System.exit(-1);
-        }
-        //computing statistics
-        try {
-            Statistics.compute(context);
-        } catch (Exception e) {
-            System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
-            LOGGER.log(Level.SEVERE, "", e);
-            System.exit(-1);
-        }
-        System.out.println("Merge files finished.");
-        return context;
-    }
+	public MergeContext mergeFiles(File left, File base, File right, String outputFilePath) {
+		FilesManager.validateFiles(left, base, right);
+		if (!isGit) {
+			System.out.println("MERGING FILES: \n" + ((left != null) ? left.getAbsolutePath() : "<empty left>") + "\n" + ((base != null) ? base.getAbsolutePath() : "<empty base>") + "\n" + ((right != null) ? right.getAbsolutePath() : "<empty right>"));
+		}
+		MergeContext context = new MergeContext(left, base, right, outputFilePath);
+		//there is no need to call specific merge algorithms in equal or consistenly changes files (fast-forward merge)
+		if (FilesManager.areFilesDifferent(left, base, right, outputFilePath, context)) {
+			try {
+				//running unstructured merge first is necessary due to future steps.
+				context.unstructuredOutput = TextualMerge.merge(left, base, right, false);
+				context.semistructuredOutput = SemistructuredMerge.merge(left, base, right, context);
+				conflictState = checkConflictState(context);
+			} catch (//textual merge must work even when semistructured not, so this exception precedes others
+					TextualMergeException //textual merge must work even when semistructured not, so this exception precedes others
+					tme) {
+				System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
+				LOGGER.log(Level.SEVERE, "", tme);
+				System.exit(-1);
+			} catch (SemistructuredMergeException sme) {
+				LOGGER.log(Level.WARNING, "", sme);
+				context.semistructuredOutput = context.unstructuredOutput;
+				conflictState = checkConflictState(context);
+			}
+		}
+		//printing the resulting merged code
+		try {
+			if (!isGit) {
+				Prettyprinter.printOnScreenMergedCode(context);
+			}
+			Prettyprinter.generateMergedFile(context, outputFilePath);
+		} catch (PrintException pe) {
+			System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
+			LOGGER.log(Level.SEVERE, "", pe);
+			System.exit(-1);
+		}
+		//computing statistics
+		try {
+			Statistics.compute(context);
+		} catch (Exception e) {
+			System.err.println("An error occurred. See " + LoggerFactory.logfile + " file for more details.\n Send the log to gjcc@cin.ufpe.br for analysis if preferable.");
+			LOGGER.log(Level.SEVERE, "", e);
+			System.exit(-1);
+		}
+		System.out.println("Merge files finished.");
+		return context;
+	}
 
-    public static void main(String[] args) {
-        JFSTMerge merger = new JFSTMerge();
-        merger.run(args);
-        System.exit(conflictState);
-    }
+	public static void main(String[] args) {
+		JFSTMerge merger = new JFSTMerge();
+		merger.run(args);
+		System.exit(conflictState);
+	}
 
-    private void run(String[] args) {
-        JCommander commandLineOptions = new JCommander(this);
-        try {
-            commandLineOptions.parse(args);
-            CommandLineValidator.validateCommandLineOptions(this);
-            if (!filespath.isEmpty()) {
-                if (filespath.size() > 3) {
-                    s3mDiff();
-                } else {
-                    mergeFiles(new File(filespath.get(0)), new File(filespath.get(1)), new File(filespath.get(2)), outputpath);
-                }
-            } else if (!directoriespath.isEmpty()) {
-                mergeDirectories(directoriespath.get(0), directoriespath.get(1), directoriespath.get(2), outputpath);
-            }
-        } catch (ParameterException pe) {
-            System.err.println(pe.getMessage());
-            commandLineOptions.setProgramName("JFSTMerge");
-            commandLineOptions.usage();
-        }
-    }
+	private void run(String[] args) {
+		JCommander commandLineOptions = new JCommander(this);
+		try {
+			commandLineOptions.parse(args);
+			CommandLineValidator.validateCommandLineOptions(this);
+			if (!filespath.isEmpty()) {
+				mergeFiles(new File(filespath.get(0)), new File(filespath.get(1)), new File(filespath.get(2)), outputpath);
+			} else if (!directoriespath.isEmpty()) {
+				mergeDirectories(directoriespath.get(0), directoriespath.get(1), directoriespath.get(2), outputpath);
+			}
+		} catch (ParameterException pe) {
+			System.err.println(pe.getMessage());
+			commandLineOptions.setProgramName("JFSTMerge");
+			commandLineOptions.usage();
+		}
+	}
 
-    private int checkConflictState(MergeContext context) {
-        List<MergeConflict> conflictList = FilesManager.extractMergeConflicts(context.semistructuredOutput);
-        if (conflictList.size() > 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+	private int checkConflictState(MergeContext context) {
+		List<MergeConflict> conflictList = FilesManager.extractMergeConflicts(context.semistructuredOutput);
+		if (conflictList.size() > 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 
-    private void s3mDiff() {
-        try {
-            ProcessBuilder builder = new ProcessBuilder("git diff", "--no-ext-diff", filespath.get(1), filespath.get(4));
-            builder.redirectErrorStream(true);
-            Process process = builder.start();
-            BufferedReader bf = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while (true) {
-                line = bf.readLine();
-                if (line == null) {
-                    break;
-                }
-                System.out.println(line);
-            }
-            System.exit(0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    ;
+
+	;
 }
