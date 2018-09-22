eb4382e62dd3459e43860f5aac101c7ee81190f1
João Victor <joao.coutinho9@gmail.com>
2018-09-22 20:35:55 -0300

    Adding test for renaming conflicts' handler and refactoring

--- src/test/java/br/ufpe/cin/mergers/handlers/RenamingConflictsHandlerTest.java
+++ src/test/java/br/ufpe/cin/mergers/handlers/RenamingConflictsHandlerTest.java
@@ -1,6 +1,6 @@
 package br.ufpe.cin.mergers.handlers;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.io.File;
 import java.io.OutputStream;
@@ -35,8 +35,8 @@ public class RenamingConflictsHandlerTest {
 				null);
 		String mergeResult = FilesManager.getStringContentIntoSingleLineNoSpacing(ctx.semistructuredOutput);
 		
-		assertTrue(mergeResult.contains("<<<<<<<MINEpublicvoidm(){inta;}=======publicvoidn(){}>>>>>>>YOURS"));
-		assertTrue(ctx.renamingConflicts == 1);
+		assertThat(mergeResult).contains("<<<<<<<MINEpublicvoidm(){inta;}=======publicvoidn(){}>>>>>>>YOURS");
+		assertThat(ctx.renamingConflicts).isOne();
 	}
 
 	@Test
@@ -48,8 +48,8 @@ public class RenamingConflictsHandlerTest {
 				null);
 		String mergeResult = FilesManager.getStringContentIntoSingleLineNoSpacing(ctx.semistructuredOutput);
 		
-		assertTrue(mergeResult.contains("<<<<<<<MINEpublicvoidm(){inta;}=======publicvoidn(){}>>>>>>>YOURS"));
-		assertTrue(ctx.renamingConflicts == 1);
+		assertThat(mergeResult).contains("<<<<<<<MINEpublicvoidm(){inta;}=======publicvoidn(){}>>>>>>>YOURS");
+		assertThat(ctx.renamingConflicts).isOne();
 	}
 	
 	@Test
@@ -61,8 +61,8 @@ public class RenamingConflictsHandlerTest {
 				null);
 		String mergeResult = FilesManager.getStringContentIntoSingleLineNoSpacing(ctx.semistructuredOutput);
 		
-		assertTrue(!mergeResult.contains("(cause:possiblerenaming)"));
-		assertTrue(ctx.renamingConflicts == 0);
+		assertThat(mergeResult).doesNotContain("(cause:possiblerenaming)");
+		assertThat(ctx.renamingConflicts).isZero();
 	}
 
 	@Test
@@ -74,8 +74,8 @@ public class RenamingConflictsHandlerTest {
 				null);
 		String mergeResult = FilesManager.getStringContentIntoSingleLineNoSpacing(ctx.semistructuredOutput);
 		
-		assertTrue(!mergeResult.contains("(cause:possiblerenaming)"));
-		assertTrue(ctx.renamingConflicts == 0);
+		assertThat(mergeResult).doesNotContain("(cause:possiblerenaming)");
+		assertThat(ctx.renamingConflicts).isZero();
 	}
 	
 	@Test
@@ -86,9 +86,26 @@ public class RenamingConflictsHandlerTest {
 				new File("testfiles/renamingmutual/right.java"),
 				null);
 		String mergeResult = FilesManager.getStringContentIntoSingleLineNoSpacing(ctx.semistructuredOutput);
-		
-		assertTrue(mergeResult.contains("<<<<<<<MINEintsum(inta,intb){returna+b;}=======intmySum(inta,intb){returna+b;}>>>>>>>YOURS"));
-		assertTrue(ctx.renamingConflicts == 1);
+
+		assertThat(mergeResult).contains("<<<<<<<MINEintsum(inta,intb){returna+b;}=======intmySum(inta,intb){returna+b;}>>>>>>>YOURS");
+		assertThat(ctx.renamingConflicts).isOne();
+	}
+
+	@Test
+	public void testRenamingConflictsParameter() {
+		boolean defaultValue = JFSTMerge.isRenamingConflictsHandlerEnabled;
+
+		JFSTMerge.isRenamingConflictsHandlerEnabled = false;
+		MergeContext ctx = 	new JFSTMerge().mergeFiles(
+				new File("testfiles/renamingmethodleftconf/left.java"),
+				new File("testfiles/renamingmethodleftconf/base.java"),
+				new File("testfiles/renamingmethodleftconf/right.java"),
+				null);
+		String mergeResult = FilesManager.getStringContentIntoSingleLineNoSpacing(ctx.semistructuredOutput);
+
+		assertThat(mergeResult).isEqualTo("publicclassTest{publicvoidn(){}<<<<<<<MINE=======publicvoidm(){inta;}>>>>>>>YOURS}");
+		assertThat(ctx.renamingConflicts).isZero();
+		JFSTMerge.isRenamingConflictsHandlerEnabled = defaultValue;
 	}
 	
 
