1cd3db682f1c2894545c24a76109c9e67588f3e4
João Victor <joao.coutinho9@gmail.com>
2018-09-30 20:34:40 -0300

    Decrypting and encrypting files between performance test

--- src/test/java/br/ufpe/cin/performance/CryptoPerformanceTest.java
+++ src/test/java/br/ufpe/cin/performance/CryptoPerformanceTest.java
@@ -1,4 +1,4 @@
-package test.java.br.ufpe.cin.performance;
+package br.ufpe.cin.performance;
 
 import static org.junit.Assert.assertTrue;
 
@@ -10,27 +10,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import static java.nio.file.StandardCopyOption.*;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import br.ufpe.cin.crypto.CryptoUtils;
+import br.ufpe.cin.exceptions.CryptoException;
+import org.junit.*;
 
 import br.ufpe.cin.app.JFSTMerge;
 
 public class CryptoPerformanceTest {
-	
-	private static final int NUM_ITERATIONS = 6;
+
 	private static final int NUM_TESTS = 5;
+	private static final int NUM_ITERATIONS = 5;
 	private static final double ACCEPTED_MARGIN = 0.3;
-	
-	public static File getLogPath(String file) {
-		return new File(System.getProperty("user.home") + File.separator + ".jfstmerge" + File.separator + file);
-	}
-	
-	public static void renameFile(String file1, String file2) throws Exception {
-		File logPath   = getLogPath(file1);
-		File logCopyPath = getLogPath(file2);
-		Files.move(logPath.toPath(), logCopyPath.toPath(), REPLACE_EXISTING);
-	}
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -41,51 +31,69 @@ public class CryptoPerformanceTest {
 			public void write(int b) {}
 		});
 		System.setOut(hideStream);
-		
-		renameFile("jfstmerge.statistics", "jfstmerge.statistics2");
     }
-	
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		renameFile("jfstmerge.statistics2", "jfstmerge.statistics");
-	}
-	
-	public long computeMeanTimeOfMergeProcedure(int testCase) {
-		long totalTime = 0;
-		for(int i = 0; i < NUM_ITERATIONS; i++) {
-			long initialTime = System.currentTimeMillis();
-					
-			new JFSTMerge().mergeFiles(
-					new File("testfiles/cryptoperformance/test" + testCase + "/left.java"), 
-					new File("testfiles/cryptoperformance/test" + testCase + "/base.java"), 
-					new File("testfiles/cryptoperformance/test" + testCase + "/right.java"),
-					null);
-			
-			long finalTime = System.currentTimeMillis();
-			totalTime += finalTime - initialTime;
-			
+
+    @Before
+	public void decryptFiles() {
+		try {
+			decrypt("jfstmerge.statistics");
+			decrypt("jfstmerge.files");
+		} catch (CryptoException e) {
+			// The files are already decrypted.
+			System.out.println("Cryptography performance test: the files are already decrypted.");
 		}
-		
-		return totalTime / NUM_ITERATIONS;
+	}
+
+	@After
+	public void encryptFiles() throws CryptoException {
+		encrypt("jfstmerge.statistics");
+		encrypt("jfstmerge.files");
 	}
 		
 	@Test
-	public void testPerformance() throws Exception {
-			
-		for(int i = 0; i < NUM_TESTS; i++) {
-			
-			JFSTMerge.isCryptographed = false;
-			long noCryptoMean = computeMeanTimeOfMergeProcedure(i);	
-		
-			JFSTMerge.isCryptographed = true;
-			long cryptoMean = computeMeanTimeOfMergeProcedure(i);
-			
-			assertTrue((double) (cryptoMean - noCryptoMean) <= ACCEPTED_MARGIN * noCryptoMean);
-			
-			// Renaming statistics file to not block next non-cryptographed merge, simplifying operations.
-			renameFile("jfstmerge.statistics", "jfstmerge.statistics2");
+	public void testPerformance() throws CryptoException {
+
+		JFSTMerge.isCryptographed = false;
+		long noCryptoMean = computeMeanTimeOfMergeProcedure(0);
+
+		JFSTMerge.isCryptographed = true;
+		long cryptoMean = computeMeanTimeOfMergeProcedure(0);
+
+		assertTrue((double) (cryptoMean - noCryptoMean) <= ACCEPTED_MARGIN * noCryptoMean);
+
+	}
+
+	private long computeMeanTimeOfMergeProcedure(int testCase) {
+		long totalTime = 0;
+		for(int i = 0; i < NUM_ITERATIONS; i++) {
+			long initialTime = System.currentTimeMillis();
+
+			new JFSTMerge().mergeFiles(
+					new File("testfiles/cryptoperformance/test" + testCase + "/left.java"),
+					new File("testfiles/cryptoperformance/test" + testCase + "/base.java"),
+					new File("testfiles/cryptoperformance/test" + testCase + "/right.java"),
+					null);
+
+			long finalTime = System.currentTimeMillis();
+			totalTime += finalTime - initialTime;
+
 		}
-		
+
+		return totalTime / NUM_ITERATIONS;
+	}
+
+	private static void decrypt(String fileName) throws CryptoException {
+		String logPath = System.getProperty("user.home") + File.separator + ".jfstmerge" + File.separator + fileName;
+
+		File file = new File(logPath);
+		CryptoUtils.decrypt(file, file);
+	}
+
+	private static void encrypt(String fileName) throws CryptoException {
+		String logPath = System.getProperty("user.home") + File.separator + ".jfstmerge" + File.separator + fileName;
+
+		File file = new File(logPath);
+		CryptoUtils.encrypt(file, file);
 	}
 	
 }
