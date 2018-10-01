783c0456944e8caefb6f9a73a317000f4c8f75a2
João Victor <joao.coutinho9@gmail.com>
2018-09-30 21:57:59 -0300

    Making cryptography performance test more generic and increasing margin

--- testfiles/cryptoperformance/test1/left.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Test {
-  class B {
-	double a;
-	double b;
-  }
-}
--- testfiles/cryptoperformance/test2/right.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Test {
-	class A {
-		double a;
-		double b;
-	}
-}
