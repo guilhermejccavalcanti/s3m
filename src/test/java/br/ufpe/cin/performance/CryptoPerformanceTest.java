ec118cc9abbfb1fed3b031baa30ee47421e18521
João Victor <joao.coutinho9@gmail.com>
2018-09-30 21:57:59 -0300

    Making cryptography performance test more generic and increasing margin

--- src/test/java/br/ufpe/cin/performance/CryptoPerformanceTest.java
+++ src/test/java/br/ufpe/cin/performance/CryptoPerformanceTest.java
@@ -1,14 +1,10 @@
 package br.ufpe.cin.performance;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.*;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import static java.nio.file.StandardCopyOption.*;
 
 import br.ufpe.cin.crypto.CryptoUtils;
 import br.ufpe.cin.exceptions.CryptoException;
@@ -18,12 +14,11 @@ import br.ufpe.cin.app.JFSTMerge;
 
 public class CryptoPerformanceTest {
 
-	private static final int NUM_TESTS = 5;
-	private static final int NUM_ITERATIONS = 5;
-	private static final double ACCEPTED_MARGIN = 0.3;
+	private static final int NUM_ITERATIONS = 6;
+	private static final double ACCEPTED_MARGIN = 0.4;
 	
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
 		//hidding sysout output
 		@SuppressWarnings("unused")
 		PrintStream originalStream = System.out;
@@ -40,43 +35,84 @@ public class CryptoPerformanceTest {
 			decrypt("jfstmerge.files");
 		} catch (CryptoException e) {
 			// The files are already decrypted.
-			System.out.println("Cryptography performance test: the files are already decrypted.");
+			System.out.println("Cryptography performance test: the files are already decrypted. Proceeding normally.");
 		}
-	}
-
-	@After
-	public void encryptFiles() throws CryptoException {
-		encrypt("jfstmerge.statistics");
-		encrypt("jfstmerge.files");
 	}
 		
 	@Test
-	public void testPerformance() throws CryptoException {
+	public void testPerformanceWithDeletion() {
 
 		JFSTMerge.isCryptographed = false;
-		long noCryptoMean = computeMeanTimeOfMergeProcedure(0);
+		long noCryptoMean = computeMeanTimeOfMergeProcedure("testfiles/deletioninnerinleft");
 
 		JFSTMerge.isCryptographed = true;
-		long cryptoMean = computeMeanTimeOfMergeProcedure(0);
+		long cryptoMean = computeMeanTimeOfMergeProcedure("testfiles/deletioninnerinleft");
 
-		assertTrue((double) (cryptoMean - noCryptoMean) <= ACCEPTED_MARGIN * noCryptoMean);
-
+		assertThat((double) (cryptoMean - noCryptoMean)).isLessThanOrEqualTo(ACCEPTED_MARGIN * noCryptoMean);
 	}
 
-	private long computeMeanTimeOfMergeProcedure(int testCase) {
-		long totalTime = 0;
+	@Test
+	public void testPerformanceWithDuplications() {
+
+		JFSTMerge.isCryptographed = false;
+		long noCryptoMean = computeMeanTimeOfMergeProcedure("testfiles/duplicationsconflicting");
+
+		JFSTMerge.isCryptographed = true;
+		long cryptoMean = computeMeanTimeOfMergeProcedure("testfiles/duplicationsconflicting");
+
+		assertThat((double) (cryptoMean - noCryptoMean)).isLessThanOrEqualTo(ACCEPTED_MARGIN * noCryptoMean);
+	}
+
+	@Test
+	public void testPerformanceWithInitializations() {
+
+		JFSTMerge.isCryptographed = false;
+		long noCryptoMean = computeMeanTimeOfMergeProcedure("testfiles/initlblocksnobase");
+
+		JFSTMerge.isCryptographed = true;
+		long cryptoMean = computeMeanTimeOfMergeProcedure("testfiles/initlblocksnobase");
+
+		assertThat((double) (cryptoMean - noCryptoMean)).isLessThanOrEqualTo(ACCEPTED_MARGIN * noCryptoMean);
+	}
+
+	@Test
+	public void testPerformanceWithNewReferencing() {
+
+		JFSTMerge.isCryptographed = false;
+		long noCryptoMean = computeMeanTimeOfMergeProcedure("testfiles/nereomethodfield");
+
+		JFSTMerge.isCryptographed = true;
+		long cryptoMean = computeMeanTimeOfMergeProcedure("testfiles/nereomethodfield");
+
+		assertThat((double) (cryptoMean - noCryptoMean)).isLessThanOrEqualTo(ACCEPTED_MARGIN * noCryptoMean);
+	}
+
+	@Test
+	public void testPerformanceWithRenaming() {
+
+		JFSTMerge.isCryptographed = false;
+		long noCryptoMean = computeMeanTimeOfMergeProcedure("testfiles/renaminginright");
+
+		JFSTMerge.isCryptographed = true;
+		long cryptoMean = computeMeanTimeOfMergeProcedure("testfiles/renaminginright");
+
+		assertThat((double) (cryptoMean - noCryptoMean)).isLessThanOrEqualTo(ACCEPTED_MARGIN * noCryptoMean);
+	}
+
+	private long computeMeanTimeOfMergeProcedure(String testFilesDirectory) {
+		long initialTime, finalTime, totalTime = 0;
+
 		for(int i = 0; i < NUM_ITERATIONS; i++) {
-			long initialTime = System.currentTimeMillis();
 
+			initialTime = System.currentTimeMillis();
 			new JFSTMerge().mergeFiles(
-					new File("testfiles/cryptoperformance/test" + testCase + "/left.java"),
-					new File("testfiles/cryptoperformance/test" + testCase + "/base.java"),
-					new File("testfiles/cryptoperformance/test" + testCase + "/right.java"),
+					new File(testFilesDirectory + "/left/Test.java"),
+					new File(testFilesDirectory + "/base/Test.java"),
+					new File(testFilesDirectory + "/right/Test.java"),
 					null);
+			finalTime = System.currentTimeMillis();
 
-			long finalTime = System.currentTimeMillis();
 			totalTime += finalTime - initialTime;
-
 		}
 
 		return totalTime / NUM_ITERATIONS;
@@ -87,13 +123,6 @@ public class CryptoPerformanceTest {
 
 		File file = new File(logPath);
 		CryptoUtils.decrypt(file, file);
-	}
-
-	private static void encrypt(String fileName) throws CryptoException {
-		String logPath = System.getProperty("user.home") + File.separator + ".jfstmerge" + File.separator + fileName;
-
-		File file = new File(logPath);
-		CryptoUtils.encrypt(file, file);
 	}
 	
 }
