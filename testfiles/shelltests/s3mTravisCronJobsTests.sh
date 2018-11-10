34f0941d7a533297cb0ba121b07a42d6d3f3ea5a
João Victor <joao.coutinho9@gmail.com>
2018-11-10 19:17:27 -0300

    Adding a test for big revision samples

--- testfiles/shelltests/s3mTravisCronJobsTests.sh
+++ testfiles/shelltests/s3mTravisCronJobsTests.sh
@@ -74,3 +74,58 @@ testBigRepository()
 	assertTrue "[ '$FN_REDUCTION' = '100.00%' ] || [ '$FN_REDUCTION' = '100,00%' ]"
 }
 
+testMergeRevisionsSample()
+{
+    # Configuration phase.
+    cp -r ${START_PATH}/sample/sampleleft/* .
+	git add .
+	git commit -m "initial commit"
+
+	git checkout -b left
+	cp -r ${START_PATH}/sample/samplebase/* .
+	git add .
+	git commit -m "left commit"
+
+	git checkout master
+	git checkout -b right
+	cp -r ${START_PATH}/sample/sampleright/* .
+	git add .
+	git commit -m "right commit"
+
+    # Merge phase.
+	git checkout master
+	git merge left --no-edit
+	git merge right --no-edit
+
+    # Evaluation phase.
+	cd $HOME/.jfstmerge
+	NUM_JAVA_FILES=$(cat jfstmerge.summary | grep -Eo "[0-9]+ JAVA files" | grep -Eo "[0-9]")
+	assertTrue "[ '$NUM_JAVA_FILES' = '3' ]"
+
+	FP_AVOIDED=$(cat jfstmerge.summary | grep -Eo "least [0-9]+ false positive\(s\)" | grep -Eo "[0-9]")
+	assertTrue "[ '$FP_AVOIDED' = '2' ]"
+
+	FN_AVOIDED=$(cat jfstmerge.summary | grep -Eo "[0-9]+ false negative\(s\)" | grep -Eo "[0-9]")
+	assertTrue "[ '$FN_AVOIDED' = '1' ]"
+
+	S3M_NUM_CONFLICTS=$(cat jfstmerge.summary | grep -Eo "reported [0-9]+ conflicts" | grep -Eo "[0-9]")
+	assertTrue "[ '$S3M_NUM_CONFLICTS' = '1' ]"
+
+	S3M_NUM_CONFLICTING_LOC=$(cat jfstmerge.summary | grep -Eo "totaling [0-9]+ conflicting" | grep -Eo "[0-9]")
+	assertTrue "[ '$S3M_NUM_CONFLICTING_LOC' = '2' ]"
+
+	UNSTR_NUM_CONFLICTS=$(cat jfstmerge.summary | grep -Eo "to [0-9]+ conflicts" | grep -Eo "[0-9]")
+	assertTrue "[ '$UNSTR_NUM_CONFLICTS' = '2' ]"
+
+	UNSTR_NUM_CONFLICTING_LOC=$(cat jfstmerge.summary | grep -Eo "and [0-9]+ conflicting" | grep -Eo "[0-9]")
+	assertTrue "[ '$UNSTR_NUM_CONFLICTING_LOC' = '5' ]"
+
+	FP_REDUCTION=$(cat jfstmerge.summary | grep -Eo "A reduction of [0-9]+[.,][0-9]+%" | grep -Eo "[0-9]+[.,][0-9]+%")
+	assertTrue "[ '$FP_REDUCTION' = '100.00%' ] || [ '$FP_REDUCTION' = '100,00%' ]"
+
+	FN_REDUCTION=$(cat jfstmerge.summary | grep -Eo "And a reduction of [0-9]+[.,][0-9]+%" | grep -Eo "[0-9]+[.,][0-9]+%")
+	assertTrue "[ '$FN_REDUCTION' = '100.00%' ] || [ '$FN_REDUCTION' = '100,00%' ]"
+
+	cat jfstmerge.summary
+}
+
