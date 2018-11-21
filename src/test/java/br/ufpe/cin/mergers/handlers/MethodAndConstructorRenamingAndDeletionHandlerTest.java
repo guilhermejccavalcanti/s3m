c0617741a7f5749b337603fb8d13d0d3b8a058f3
gaabs <gaabs@cin.ufpe.br>
2018-11-21 10:46:01 -0300

    Add Mutual Renaming regression tests

--- src/test/java/br/ufpe/cin/mergers/handlers/MethodAndConstructorRenamingAndDeletionHandlerTest.java
+++ src/test/java/br/ufpe/cin/mergers/handlers/MethodAndConstructorRenamingAndDeletionHandlerTest.java
@@ -14,13 +14,14 @@ public class MethodAndConstructorRenamingAndDeletionHandlerTest {
     private File baseFile = new File("testfiles/renaming/method/base_method/Test.java");
     private File renamedMethodFile1 = new File("testfiles/renaming/method/renamed_method_1/Test.java");
     private File renamedMethodFile2 = new File("testfiles/renaming/method/renamed_method_2/Test.java");
+    private File addedMethodFile1 = new File("testfiles/renaming/method/added_method_1/Test.java");
+    private File addedMethodFile2 = new File("testfiles/renaming/method/added_method_2/Test.java");
 
     private JFSTMerge jfstMerge = new JFSTMerge();
     private MergeContext mergeContext;
 
     @BeforeClass
     public static void setUpBeforeClass() throws UnsupportedEncodingException {
-        //hidding sysout output
         TestUtils.hideSystemOutput();
     }
 
@@ -33,6 +34,29 @@ public class MethodAndConstructorRenamingAndDeletionHandlerTest {
 
         merge(renamedMethodFile2, renamedMethodFile1);
         TestUtils.verifyMergeResultWithRenamingConflict(mergeContext, "<<<<<<<MINEpublicvoidn2(){inta;}=======publicvoidn1(){inta;}>>>>>>>YOURS");
+    }
+
+    @Test
+    public void testMutualMethodRenaming_whenSomeVersionHasNotRenamedAnyMethodsOrConstructors_shouldNotReportConflict() {
+        JFSTMerge.renamingStrategy = RenamingStrategy.SAFE;
+
+        merge(addedMethodFile1, addedMethodFile2);
+        TestUtils.verifyMergeResultWithoutRenamingConflict(mergeContext, "publicvoidn1(){inta;}publicvoidn2(){inta;}");
+
+        merge(addedMethodFile2, addedMethodFile1);
+        TestUtils.verifyMergeResultWithoutRenamingConflict(mergeContext, "publicvoidn2(){inta;}publicvoidn1(){inta;}");
+
+        merge(renamedMethodFile1, addedMethodFile2);
+        TestUtils.verifyMergeResultWithoutRenamingConflict(mergeContext, "publicvoidn1(){inta;}publicvoidn2(){inta;}");
+
+        merge(addedMethodFile2, renamedMethodFile1);
+        TestUtils.verifyMergeResultWithoutRenamingConflict(mergeContext, "publicvoidn2(){inta;}publicvoidn1(){inta;}");
+
+        merge(addedMethodFile1, renamedMethodFile2);
+        TestUtils.verifyMergeResultWithoutRenamingConflict(mergeContext, "publicvoidn1(){inta;}publicvoidn2(){inta;}");
+
+        merge(renamedMethodFile2, addedMethodFile1);
+        TestUtils.verifyMergeResultWithoutRenamingConflict(mergeContext, "publicvoidn2(){inta;}publicvoidn1(){inta;}");
     }
 
     @Test
