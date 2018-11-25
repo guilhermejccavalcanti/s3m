4ac7603e38fcc298be795b5a6129fb6d84667d3f
Arthurlpgc <arthurlpgcosta@gmail.com>
2018-11-25 19:57:19 -0300

    rename with rminer integration: first working version

--- src/main/java/br/ufpe/cin/mergers/handlers/MethodAndConstructorRenamingAndDeletionHandler.java
+++ src/main/java/br/ufpe/cin/mergers/handlers/MethodAndConstructorRenamingAndDeletionHandler.java
@@ -7,8 +7,14 @@ import br.ufpe.cin.mergers.util.MergeContext;
 import br.ufpe.cin.mergers.util.RenamingStrategy;
 import de.ovgu.cide.fstgen.ast.FSTNode;
 import de.ovgu.cide.fstgen.ast.FSTTerminal;
+import gr.uom.java.xmi.UMLModel;
+import gr.uom.java.xmi.UMLModelASTReader;
+import gr.uom.java.xmi.diff.UMLModelDiff;
 import org.apache.commons.lang3.tuple.Pair;
+import org.refactoringminer.api.Refactoring;
+import org.refactoringminer.api.RefactoringType;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -27,12 +33,60 @@ public final class MethodAndConstructorRenamingAndDeletionHandler implements Con
 
     private enum RenamingSide {LEFT, RIGHT}
 
+    private ArrayList<String> getFileNames(File file, boolean firstLevel){
+        ArrayList<String> ret = new ArrayList<>();
+        if (file != null) {
+            if (file.isDirectory()){
+                File[] files = file.listFiles();
+                for (File childFile: files) {
+                    ArrayList<String> aux = getFileNames(childFile, false);
+                    for(String str: aux){
+                        if (firstLevel)
+                            ret.add("/" + str);
+                        else
+                            ret.add(file.getName() + "/" + str);
+                    }
+                }
+            } else {
+                ret.add(file.getName());
+            }
+        }
+        return ret;
+
+    }
+
+    private boolean hasRenaming(File base, File after){
+        if(base.isFile()) base = base.getParentFile();
+        ArrayList<String> baseFiles = getFileNames(base, true);
+
+        if(after.isFile()) after = after.getParentFile();
+        ArrayList<String> afterFiles = getFileNames(after, true);
+
+
+        UMLModel model1 = new UMLModelASTReader(base, baseFiles).getUmlModel();
+        UMLModel model2 = new UMLModelASTReader(after, afterFiles).getUmlModel();
+        UMLModelDiff modelDiff = model1.diff(model2);
+        List<Refactoring> refactorings = modelDiff.getRefactorings();
+        boolean ret = false;
+        for (int i = 0; i < refactorings.size(); i++){
+            if(refactorings.get(i).getRefactoringType() == RefactoringType.RENAME_METHOD) ret = true;
+        }
+        System.out.println("ret");
+        System.out.println(ret);
+        return ret;
+    }
+
     public void handle(MergeContext context) {
+        boolean leftRenaming = hasRenaming(context.getBase(), context.getLeft());
+        boolean rightRenaming = hasRenaming(context.getBase(), context.getRight());
+
         //when both developers rename the same method/constructor
-        handleMutualRenamings(context);
+        if (leftRenaming && rightRenaming)
+            handleMutualRenamings(context);
 
         //when one of the developers rename a method/constructor
-        handleSingleRenamings(context);
+        if (leftRenaming || rightRenaming)
+            handleSingleRenamings(context);
     }
 
     private static void handleMutualRenamings(MergeContext context) {
