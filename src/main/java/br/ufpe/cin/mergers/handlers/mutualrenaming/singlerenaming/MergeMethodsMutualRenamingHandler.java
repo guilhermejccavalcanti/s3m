e963394b4d359d842d26cfe914b92371671d5471
gaabs <gaabs@cin.ufpe.br>
2018-11-28 19:47:54 -0300

    Extract anonymous functions at MergeMethodsMutualRenamingHandler

--- src/main/java/br/ufpe/cin/mergers/handlers/mutualrenaming/singlerenaming/MergeMethodsMutualRenamingHandler.java
+++ src/main/java/br/ufpe/cin/mergers/handlers/mutualrenaming/singlerenaming/MergeMethodsMutualRenamingHandler.java
@@ -9,22 +9,20 @@ import java.util.List;
 
 public class MergeMethodsMutualRenamingHandler implements MutualRenamingHandler {
     public void handle(MergeContext context) {
-        List<FSTNode> mutualRemovedMethodsOrConstructors = RenamingUtils.getMethodsOrConstructors(context.deletedBaseNodes);
+        List<FSTNode> mutuallyRemovedMethodsOrConstructors = RenamingUtils.getMethodsOrConstructors(context.deletedBaseNodes);
         List<FSTNode> leftNewMethodsOrConstructors = RenamingUtils.getMethodsOrConstructors(context.addedLeftNodes);
         List<FSTNode> rightNewMethodsOrConstructors = RenamingUtils.getMethodsOrConstructors(context.addedRightNodes);
 
-        if (mutualRemovedMethodsOrConstructors.isEmpty()) return;
+        if (mutuallyRemovedMethodsOrConstructors.isEmpty()) return;
         if (leftNewMethodsOrConstructors.isEmpty() || rightNewMethodsOrConstructors.isEmpty()) return;
 
-        mutualRemovedMethodsOrConstructors.stream()
+        mutuallyRemovedMethodsOrConstructors.stream()
                 .map(node -> getNodesWithMatchingBody(node, leftNewMethodsOrConstructors, rightNewMethodsOrConstructors))
-                .filter(pair -> pair.getLeft() != null && pair.getRight() != null)
-                .filter(pair -> RenamingUtils.haveDifferentSignature(pair.getLeft(), pair.getRight()))
-                .forEach(pair -> {
-                    RenamingUtils.generateMutualRenamingConflict(context, pair.getLeft(), pair.getRight());
-                    leftNewMethodsOrConstructors.remove(pair.getLeft());
-                    rightNewMethodsOrConstructors.remove(pair.getRight());
-                });
+                .filter(this::hasFoundMatchingNodes)
+                .filter(this::hasConflict)
+                .forEach(pair -> reportConflictAndUpdateNodesLists(context, pair.getLeft(), pair.getRight(),
+                        leftNewMethodsOrConstructors, rightNewMethodsOrConstructors)
+                );
     }
 
     private Pair<FSTNode, FSTNode> getNodesWithMatchingBody(FSTNode baseRemovedNode, List<FSTNode> leftNodes,
@@ -40,5 +38,21 @@ public class MergeMethodsMutualRenamingHandler implements MutualRenamingHandler 
                 .orElse(null);
 
         return Pair.of(leftNode, rightNode);
+    }
+
+    private boolean hasFoundMatchingNodes(Pair<FSTNode, FSTNode> nodePair) {
+        return nodePair.getLeft() != null && nodePair.getRight() != null;
+    }
+
+    private boolean hasConflict(Pair<FSTNode, FSTNode> nodePair) {
+        return RenamingUtils.haveDifferentSignature(nodePair.getLeft(), nodePair.getRight());
+    }
+
+    private void reportConflictAndUpdateNodesLists(MergeContext context, FSTNode leftNode, FSTNode rightNode,
+                                                   List<FSTNode> leftNewMethodsOrConstructors,
+                                                   List<FSTNode> rightNewMethodsOrConstructors) {
+        RenamingUtils.generateMutualRenamingConflict(context, leftNode, rightNode);
+        leftNewMethodsOrConstructors.remove(leftNode);
+        rightNewMethodsOrConstructors.remove(rightNode);
     }
 }
