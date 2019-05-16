aff77429e5ef4c29cbb474720c435eba47e054b1
Alice Borner <azbo@cin.ufpe.br>
2019-05-15 22:34:29 -0300

    Adding new test files.

---
+++ testfiles/initlblocksnewapproach/multipleblocksrenamingvariables/base/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+	
+	static {
+		int a = 3;
+		int b = 2;
+	}
+
+	static {
+		int c = 2;
+		int d = 3;
+	}
+}
---
+++ testfiles/initlblocksnewapproach/multipleblocksrenamingvariables/left/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+	
+	static {
+		int a = 4;
+		int b = 2;
+	}
+
+	static {
+		int c = 2;
+		int var_d = 3;
+	}
+}
