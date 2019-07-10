6f4cd1c5e366473623c6e8a43c76ced75ce37972
João Victor <joao.coutinho9@gmail.com>
2019-07-10 13:34:17 -0300

    Displaying BASE word in conflict

--- src/main/java/br/ufpe/cin/mergers/util/MergeConflict.java
+++ src/main/java/br/ufpe/cin/mergers/util/MergeConflict.java
@@ -29,7 +29,7 @@ public class MergeConflict {
 		this.right = rightConflictingContent;
 		this.body = "<<<<<<< MINE\n" + 
 					leftConflictingContent + 
-					"\n|||||||\n" +
+					"\n||||||| BASE\n" +
 					baseConflictingContent +
 					"\n=======\n" +
 					rightConflictingContent +
