04ba57e37eb60eb576a46ab7c8991dab2fd0f4c7
João Victor <joao.coutinho9@gmail.com>
2019-08-13 22:26:48 -0300

    Adding new tests for comments

--- testfiles/comments/addandremovecomments/base/Test.java
+++ testfiles/comments/addandremovecomments/base/Test.java
@@ -1,4 +1,5 @@
 public class Test {
 
+    int a;
 
 }
---
+++ testfiles/comments/editedcomment/right/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    /**
+     * Method m
+     * @param a
+     */
+    public void m(int a) {
+        int b;
+    }
+
+    int c;
+
+
+}
