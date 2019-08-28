b6b792c4671b2e965f576835d83ab98b1d96e3e8
João Victor <joao.coutinho9@gmail.com>
2019-08-28 09:52:11 -0300

    Refactoring on alterInherits

--- src/main/java/br/ufpe/cin/generator/Generator.java
+++ src/main/java/br/ufpe/cin/generator/Generator.java
@@ -39,22 +39,26 @@ public class Generator {
 					"grammars/java18_merge_fst_test.java"
 					);
 		
-					alterInherits();
+					alterInheritance();
 		}catch(Exception e){
 			System.err.println("Refresh the root project folder (F5). Try again.");
 			e.printStackTrace();
 		}
 	}
 
-	private static void alterInherits() throws IOException {
+	private static void alterInheritance() throws IOException {
+		alterSimplePrintVisitorInheritance();
+	}
+
+	private static void alterSimplePrintVisitorInheritance() throws IOException {
 		Path path = Paths.get("src/main/java/br/ufpe/cin/generated/SimplePrintVisitor.java");
 		List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
 
 		for (int i = 0; i < lines.size(); i++) {
-			if (lines.get(i).contains("extends AbstractFSTPrintVisitor")) {
-				lines.set(i, "public class SimplePrintVisitor extends S3MPrettyPrinter {");
-				lines.set(i - 1, "import br.ufpe.cin.printers.S3MPrettyPrinter;");
-				for (int j = 1; j <= 6; j++) {
+			if (lines.get(i).contains("extends AbstractFSTPrintVisitor")) { // when we find the inheritance line
+				lines.set(i, "public class SimplePrintVisitor extends S3MPrettyPrinter {"); // change the inheritance
+				lines.set(i - 1, "import br.ufpe.cin.printers.S3MPrettyPrinter;"); // add an import to our class one line above
+				for (int j = 1; j <= 6; j++) { // erase the next 6 lines, where there are unnecessary constructors
 					lines.set(i + j, "");
 				}
 				break;
