c2a7b49b0c65af99dc6804a84ad18002b558f2c5
jvcoutinho <joao.coutinho9@gmail.com>
2019-09-28 19:13:14 -0300

    Removing assertions

--- src/main/java/br/ufpe/cin/app/JFSTMerge.java
+++ src/main/java/br/ufpe/cin/app/JFSTMerge.java
@@ -196,8 +196,6 @@ public class JFSTMerge {
 
 		if(isEncodingInferenceEnabled) {
 			FilesEncoding.analyseFiles(left, base, right);
-			assert(FilesEncoding.retrieveEncoding(left).equals(FilesEncoding.retrieveEncoding(base)));
-			assert(FilesEncoding.retrieveEncoding(base).equals(FilesEncoding.retrieveEncoding(right)));
 		}
 
 		if (!isGit) {
