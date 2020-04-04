3faa0c52badbb7b393db77e3f85f5fe373918a0c
Guilherme Cavalcanti <gjcc@cin.ufpe.br>
2020-04-04 18:41:32 -0300

    filter method/constructor nodes from base tree on renaming/del handlers

--- src/main/java/br/ufpe/cin/mergers/handlers/MethodAndConstructorRenamingAndDeletionHandler.java
+++ src/main/java/br/ufpe/cin/mergers/handlers/MethodAndConstructorRenamingAndDeletionHandler.java
@@ -38,165 +38,166 @@ import java.util.stream.Collectors;
  * @author Giovanni Barros (gaabs@cin.ufpe.br)
  */
 public final class MethodAndConstructorRenamingAndDeletionHandler implements ConflictHandler {
-    private RenamingHandler renamingHandler;
+	private RenamingHandler renamingHandler;
 
-    public MethodAndConstructorRenamingAndDeletionHandler() {
-        this.renamingHandler = RenamingHandlerFactory.getHandler(JFSTMerge.renamingStrategy);
-    }
+	public MethodAndConstructorRenamingAndDeletionHandler() {
+		this.renamingHandler = RenamingHandlerFactory.getHandler(JFSTMerge.renamingStrategy);
+	}
 
-    @Override
-    public void handle(MergeContext context) throws TextualMergeException {
+	@Override
+	public void handle(MergeContext context) throws TextualMergeException {
 
-        /*
-         * 1. Identification Step: Traverses the base tree looking for renamed or
-         * deleted nodes, filling the renamedWithoutBodyChanges and
-         * deletedOrRenamedWithoutBodyChanges attributes.
-         */
-        identifyRenamingOrDeletionNodes(context);
+		/*
+		 * 1. Identification Step: Traverses the base tree looking for renamed or
+		 * deleted nodes, filling the renamedWithoutBodyChanges and
+		 * deletedOrRenamedWithoutBodyChanges attributes.
+		 */
+		identifyRenamingOrDeletionNodes(context);
 
-        /*
-         * 2. Match Step: For each of the base renamed nodes, we find the nodes from the
-         * left, right and merge trees involved in this renaming. If we can't find the
-         * left or the right node, we treat them as deleted.
-         */
-        List<Quartet<FSTNode, FSTNode, FSTNode, FSTNode>> renamingMatches = retrieveRenamingMatches(context);
+		/*
+		 * 2. Match Step: For each of the base renamed nodes, we find the nodes from the
+		 * left, right and merge trees involved in this renaming. If we can't find the
+		 * left or the right node, we treat them as deleted.
+		 */
+		List<Quartet<FSTNode, FSTNode, FSTNode, FSTNode>> renamingMatches = retrieveRenamingMatches(context);
 
-        /*
-         * 3. Handling Step: For each of the matches, we simply run a decision tree to
-         * decide what do to with them, based on their renaming types.
-         */
-        handle(context, renamingMatches);
+		/*
+		 * 3. Handling Step: For each of the matches, we simply run a decision tree to
+		 * decide what do to with them, based on their renaming types.
+		 */
+		handle(context, renamingMatches);
 
-    }
+	}
 
-    private void identifyRenamingOrDeletionNodes(MergeContext context) {
-        List<FSTTerminal> terminals = Traverser.collectTerminals(context.baseTree);
-        for (FSTTerminal terminal : terminals) {
-            identifyRenamingOrDeletion(Side.LEFT, context, terminal, context.leftTree, context.addedLeftNodes);
-            identifyRenamingOrDeletion(Side.RIGHT, context, terminal, context.rightTree, context.addedRightNodes);
-        }
-    }
+	private void identifyRenamingOrDeletionNodes(MergeContext context) {
+		List<FSTTerminal> terminals = Traverser.collectTerminals(context.baseTree);
+		terminals.stream().filter(terminal -> RenamingUtils.isMethodOrConstructorNode(terminal))
+		.forEach(terminal -> {
+			identifyRenamingOrDeletion(Side.LEFT, context, terminal, context.leftTree, context.addedLeftNodes);
+			identifyRenamingOrDeletion(Side.RIGHT, context, terminal, context.rightTree, context.addedRightNodes);
+		});
+	}
 
-    private void identifyRenamingOrDeletion(Side contribution, MergeContext context, FSTNode node,
-            FSTNode contributionTree, List<FSTNode> addedNodes) {
+	private void identifyRenamingOrDeletion(Side contribution, MergeContext context, FSTNode node,
+			FSTNode contributionTree, List<FSTNode> addedNodes) {
 
-        if (isRenamingWithoutBodyChanges(node, contributionTree, addedNodes)) {
-            context.renamedWithoutBodyChanges.add(Pair.of(contribution, node));
-        }
+		if (isRenamingWithoutBodyChanges(node, contributionTree, addedNodes)) {
+			context.renamedWithoutBodyChanges.add(Pair.of(contribution, node));
+		}
 
-        if (isDeletionOrRenamingWithBodyChanges(node, contributionTree, addedNodes)) {
-            context.deletedOrRenamedWithBodyChanges.add(Pair.of(contribution, node));
-        }
-    }
+		if (isDeletionOrRenamingWithBodyChanges(node, contributionTree, addedNodes)) {
+			context.deletedOrRenamedWithBodyChanges.add(Pair.of(contribution, node));
+		}
+	}
 
-    private boolean isRenamingWithoutBodyChanges(FSTNode node, FSTNode contributionTree, List<FSTNode> addedNodes) {
-        return !isInContribution(node, contributionTree) && matchesWithEqualBody(node, addedNodes);
-    }
+	private boolean isRenamingWithoutBodyChanges(FSTNode node, FSTNode contributionTree, List<FSTNode> addedNodes) {
+		return !isInContribution(node, contributionTree) && matchesWithEqualBody(node, addedNodes);
+	}
 
-    private boolean isDeletionOrRenamingWithBodyChanges(FSTNode node, FSTNode contributionTree,
-            List<FSTNode> addedNodes) {
-        return !isInContribution(node, contributionTree) && !matchesWithEqualBody(node, addedNodes);
-    }
+	private boolean isDeletionOrRenamingWithBodyChanges(FSTNode node, FSTNode contributionTree,
+			List<FSTNode> addedNodes) {
+		return !isInContribution(node, contributionTree) && !matchesWithEqualBody(node, addedNodes);
+	}
 
-    private boolean isInContribution(FSTNode node, FSTNode contributionTree) {
-        return Traverser.isInTree(node, contributionTree);
-    }
+	private boolean isInContribution(FSTNode node, FSTNode contributionTree) {
+		return Traverser.isInTree(node, contributionTree);
+	}
 
-    private boolean matchesWithEqualBody(FSTNode baseNode, List<FSTNode> addedNodes) {
-        return addedNodes.stream().filter(node -> node instanceof FSTTerminal)
-                .anyMatch(node -> RenamingUtils.haveEqualBodyModuloWhitespace(baseNode, node));
-    }
+	private boolean matchesWithEqualBody(FSTNode baseNode, List<FSTNode> addedNodes) {
+		return addedNodes.stream().filter(node -> node instanceof FSTTerminal)
+				.anyMatch(node -> RenamingUtils.haveEqualBodyModuloWhitespace(baseNode, node));
+	}
 
-    private List<Quartet<FSTNode, FSTNode, FSTNode, FSTNode>> retrieveRenamingMatches(MergeContext context) {
-        List<Pair<Side, FSTNode>> renamedNodes = unionRenamedNodes(context.renamedWithoutBodyChanges,
-                context.deletedOrRenamedWithBodyChanges);
-        return renamedNodes.stream().map(pair -> retrieveScenarioNodes(context, pair.getRight()))
-                .filter(triple -> triple.getLeft() != null || triple.getRight() != null)
-                .map(triple -> retrieveScenarioNodesWithMergeNode(context, triple)).distinct()
-                .collect(Collectors.toList());
-    }
+	private List<Quartet<FSTNode, FSTNode, FSTNode, FSTNode>> retrieveRenamingMatches(MergeContext context) {
+		List<Pair<Side, FSTNode>> renamedNodes = unionRenamedNodes(context.renamedWithoutBodyChanges,
+				context.deletedOrRenamedWithBodyChanges);
+		return renamedNodes.stream().map(pair -> retrieveScenarioNodes(context, pair.getRight()))
+				.filter(triple -> triple.getLeft() != null || triple.getRight() != null)
+				.map(triple -> retrieveScenarioNodesWithMergeNode(context, triple)).distinct()
+				.collect(Collectors.toList());
+	}
 
-    private Triple<FSTNode, FSTNode, FSTNode> retrieveScenarioNodes(MergeContext context, FSTNode baseNode) {
-        return Triple.of(getMostAccurateMatch(baseNode, context.leftTree), baseNode,
-                getMostAccurateMatch(baseNode, context.rightTree));
-    }
+	private Triple<FSTNode, FSTNode, FSTNode> retrieveScenarioNodes(MergeContext context, FSTNode baseNode) {
+		return Triple.of(getMostAccurateMatch(baseNode, context.leftTree), baseNode,
+				getMostAccurateMatch(baseNode, context.rightTree));
+	}
 
-    private Quartet<FSTNode, FSTNode, FSTNode, FSTNode> retrieveScenarioNodesWithMergeNode(MergeContext context,
-            Triple<FSTNode, FSTNode, FSTNode> scenarioNodes) {
-        FSTNode leftNode = scenarioNodes.getLeft();
-        FSTNode baseNode = scenarioNodes.getMiddle();
-        FSTNode rightNode = scenarioNodes.getRight();
+	private Quartet<FSTNode, FSTNode, FSTNode, FSTNode> retrieveScenarioNodesWithMergeNode(MergeContext context,
+			Triple<FSTNode, FSTNode, FSTNode> scenarioNodes) {
+		FSTNode leftNode = scenarioNodes.getLeft();
+		FSTNode baseNode = scenarioNodes.getMiddle();
+		FSTNode rightNode = scenarioNodes.getRight();
 
-        if (leftNode != null)
-            return Quartet.with(leftNode, baseNode, rightNode,
-                    getCorrespondentNode(leftNode, context.superImposedTree));
-        else
-            return Quartet.with(leftNode, baseNode, rightNode,
-                    getCorrespondentNode(rightNode, context.superImposedTree));
-    }
+		if (leftNode != null)
+			return Quartet.with(leftNode, baseNode, rightNode,
+					getCorrespondentNode(leftNode, context.superImposedTree));
+		else
+			return Quartet.with(leftNode, baseNode, rightNode,
+					getCorrespondentNode(rightNode, context.superImposedTree));
+	}
 
-    private FSTNode getCorrespondentNode(FSTNode node, FSTNode tree) {
-        return Traverser.retrieveNodeFromTree(node, tree);
-    }
+	private FSTNode getCorrespondentNode(FSTNode node, FSTNode tree) {
+		return Traverser.retrieveNodeFromTree(node, tree);
+	}
 
-    private FSTNode getMostAccurateMatch(FSTNode node, FSTNode contributionTree) {
-        List<FSTTerminal> terminals = Traverser.collectTerminals(contributionTree);
-        if (RenamingUtils.isAbstractMethod(node)) {
-            return getMostAccurateMatchForAbstractMethods(node, terminals);
-        } else {
-            return getMostAccurateMatchForNonAbstractMethods(node, terminals);
-        }
-    }
+	private FSTNode getMostAccurateMatch(FSTNode node, FSTNode contributionTree) {
+		List<FSTTerminal> terminals = Traverser.collectTerminals(contributionTree);
+		if (RenamingUtils.isAbstractMethod(node)) {
+			return getMostAccurateMatchForAbstractMethods(node, terminals);
+		} else {
+			return getMostAccurateMatchForNonAbstractMethods(node, terminals);
+		}
+	}
 
-    private FSTNode getMostAccurateMatchForAbstractMethods(FSTNode node, List<FSTTerminal> terminals) {
-        FSTNode match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualSignature(n1, n2));
-        if (match == null) {
-            match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualSignatureButName(n1, n2)
-                    || RenamingUtils.haveEqualSignatureButArguments(n1, n2));
-        }
-        return match;
-    }
+	private FSTNode getMostAccurateMatchForAbstractMethods(FSTNode node, List<FSTTerminal> terminals) {
+		FSTNode match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualSignature(n1, n2));
+		if (match == null) {
+			match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualSignatureButName(n1, n2)
+					|| RenamingUtils.haveEqualSignatureButArguments(n1, n2));
+		}
+		return match;
+	}
 
-    private FSTNode getMostAccurateMatchForNonAbstractMethods(FSTNode node, List<FSTTerminal> terminals) {
-        FSTNode match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualSignature(n1, n2));
-        if (match == null) {
-            match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualBodyModuloWhitespace(n1, n2));
-        }
-        if (match == null) {
-            match = tryFindFirstMatch(terminals, node,
-                    (n1, n2) -> RenamingUtils.haveSimilarBodyModuloWhitespace(n1, n2)
-                            && (RenamingUtils.haveEqualSignatureButName(n1, n2)
-                                    || RenamingUtils.haveEqualSignatureButArguments(n1, n2)));
-        }
-        if (match == null) {
-            match = tryFindFirstMatch(terminals, node,
-                    (n1, n2) -> RenamingUtils.oneContainsTheBodyFromTheOther(n1, n2));
-        }
-        return match;
-    }
+	private FSTNode getMostAccurateMatchForNonAbstractMethods(FSTNode node, List<FSTTerminal> terminals) {
+		FSTNode match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualSignature(n1, n2));
+		if (match == null) {
+			match = tryFindFirstMatch(terminals, node, (n1, n2) -> RenamingUtils.haveEqualBodyModuloWhitespace(n1, n2));
+		}
+		if (match == null) {
+			match = tryFindFirstMatch(terminals, node,
+					(n1, n2) -> RenamingUtils.haveSimilarBodyModuloWhitespace(n1, n2)
+					&& (RenamingUtils.haveEqualSignatureButName(n1, n2)
+							|| RenamingUtils.haveEqualSignatureButArguments(n1, n2)));
+		}
+		if (match == null) {
+			match = tryFindFirstMatch(terminals, node,
+					(n1, n2) -> RenamingUtils.oneContainsTheBodyFromTheOther(n1, n2));
+		}
+		return match;
+	}
 
-    private FSTNode tryFindFirstMatch(List<FSTTerminal> candidates, FSTNode node,
-            BiPredicate<FSTNode, FSTNode> predicate) {
-        for (FSTNode candidate : candidates) {
-            if (RenamingUtils.isMethodOrConstructorNode(candidate) && predicate.test(node, candidate)) {
-                return candidate;
-            }
-        }
-        return null;
-    }
+	private FSTNode tryFindFirstMatch(List<FSTTerminal> candidates, FSTNode node,
+			BiPredicate<FSTNode, FSTNode> predicate) {
+		for (FSTNode candidate : candidates) {
+			if (RenamingUtils.isMethodOrConstructorNode(candidate) && predicate.test(node, candidate)) {
+				return candidate;
+			}
+		}
+		return null;
+	}
 
-    private List<Pair<Side, FSTNode>> unionRenamedNodes(List<Pair<Side, FSTNode>> renamedWithoutBodyChanges,
-            List<Pair<Side, FSTNode>> deletedOrRenamedWithBodyChanges) {
-        List<Pair<Side, FSTNode>> unionNodes = new ArrayList<>();
-        renamedWithoutBodyChanges.stream().forEach(pair -> unionNodes.add(pair));
-        deletedOrRenamedWithBodyChanges.stream().forEach(pair -> unionNodes.add(pair));
-        return unionNodes;
-    }
+	private List<Pair<Side, FSTNode>> unionRenamedNodes(List<Pair<Side, FSTNode>> renamedWithoutBodyChanges,
+			List<Pair<Side, FSTNode>> deletedOrRenamedWithBodyChanges) {
+		List<Pair<Side, FSTNode>> unionNodes = new ArrayList<>();
+		renamedWithoutBodyChanges.stream().forEach(pair -> unionNodes.add(pair));
+		deletedOrRenamedWithBodyChanges.stream().forEach(pair -> unionNodes.add(pair));
+		return unionNodes;
+	}
 
-    private void handle(MergeContext context, List<Quartet<FSTNode, FSTNode, FSTNode, FSTNode>> renamingMatches)
-            throws TextualMergeException {
-        for (Quartet<FSTNode, FSTNode, FSTNode, FSTNode> match : renamingMatches)
-            renamingHandler.handle(context, match);
-    }
+	private void handle(MergeContext context, List<Quartet<FSTNode, FSTNode, FSTNode, FSTNode>> renamingMatches)
+			throws TextualMergeException {
+		for (Quartet<FSTNode, FSTNode, FSTNode, FSTNode> match : renamingMatches)
+			renamingHandler.handle(context, match);
+	}
 
 }
